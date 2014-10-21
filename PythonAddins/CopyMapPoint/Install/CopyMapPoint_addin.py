8dfdc8eb19be93eeecff63db73542c43a0e070d1
jason <jason@voyagersearch.com>
2014-10-20 20:07:47 -0700

    Updated CopyMapPoint code

--- PythonAddins/CopyMapPoint/Install/CopyMapPoint_addin.py
+++ PythonAddins/CopyMapPoint/Install/CopyMapPoint_addin.py
@@ -1,4 +1,25 @@
-"""CopyMapPoint ArcMap Tool"""
+# The MIT License (MIT)
+
+# Copyright (c) [2014] [GIS Professional Development, gispd.com]
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import functools
 import os
 import threading
@@ -21,8 +42,8 @@ def dd_to_dms(dd):
     return int(degrees), int(minutes), seconds
 
 
+# A decorator that will run its wrapped function in a new thread
 def run_in_other_thread(function):
-    """A decorator that will run its wrapped function in a new thread."""
     # functool.wraps will copy over the docstring and some other metadata from the original function
     @functools.wraps(function)
     def fn_(*args, **kwargs):
@@ -40,8 +61,12 @@ class CopyMapPoint(object):
 
     def onMouseDownMap(self, x, y, button, shift):
         """Copies map x,y to the clip board in degrees, minutes, seconds."""
+
+        # Get the spatial reference from the data frame.
         mxd = arcpy.mapping.MapDocument('current')
         map_sr = mxd.activeDataFrame.spatialReference
+
+        # Get the clicked point and reproject it.
         map_point = arcpy.PointGeometry(arcpy.Point(x, y), map_sr)
         wgs84_sr = arcpy.SpatialReference(4326)
         transformation = arcpy.ListTransformations(map_sr, wgs84_sr)
@@ -50,6 +75,7 @@ class CopyMapPoint(object):
         else:
             wgs84_pt = map_point.projectAs(wgs84_sr)
 
+         # Set the hemisphere indicators.
         if wgs84_pt.firstPoint.X > 0:
             east_or_west = 'E'
         else:
@@ -58,9 +84,13 @@ class CopyMapPoint(object):
             south_or_north = 'S'
         else:
             south_or_north = 'N'
+
+        # Get the lat/long values in the required format.
         x_dms = dd_to_dms(wgs84_pt.firstPoint.X)
         y_dms = dd_to_dms(wgs84_pt.firstPoint.Y)
         add_to_clip_board("""{} {} {}{}  {} {} {}{}""".format(x_dms[0], x_dms[1], x_dms[2], east_or_west, y_dms[0], y_dms[1], y_dms[2], south_or_north))
+
         # Our new wrapped versions of os.startfile and webbrowser.open startfile = run_in_other_thread(os.startfile)
         open_browser = run_in_other_thread(webbrowser.open)
         open_browser("www.maps.google.com")
+
--- PythonAddins/CopyMapPoint/README.txt
+++ PythonAddins/CopyMapPoint/README.txt
@@ -1,11 +1,38 @@
-This is the CopyMapPoint project created by the ArcGIS Desktop Python AddIn Wizard. The CopyMapPoint_addin.py file includes the source code for this
-AddIn.
+GISPD.com Sample Code
 
-This sample AddIn is an ArcMap tool that copies the map x, y coordinates as degrees, minutes, seconds to the clipboard.
-The coordinates can be copied into maps.google.com. 
+Disclaimer: These resources are intended as samples and learning tools only.
 
-For more information about creating AddIns with Python, see the ArcGIS Guide book for building AddIns with Python:
-http://resources.arcgis.com/en/help/main/10.2/#/What_is_a_Python_add_in/014p00000025000000/
+Authors:
+	Jason Pardy, NewfoundGEO Consulting - newfoundgeo.com & gispd.com
+	David A. Howes, David Howes, LLC - dhowes.com & gispd.com
+
+Creation date: October 20, 2014
+
+Type: Esri ArcMap 10.2.2 Python add-in
+
+Development environment:
+	Esri Python Add-In Wizard - http://www.arcgis.com/home/item.html?id=5f3aefe77f6b4f61ad3e4c62f30bff3b#!
+	PyScripter 2.5.3.0 x64 - https://code.google.com/p/pyscripter/
+
+Purpose:
+ArcMap add-in tool to allow the user to click on the map and obtain a lat/long string representing the clicked location. The string can be used in Google or Bing Maps, for example.
+
+Installation:
+Double click on CopyMapPoint.esriaddin - this will install the add-in "GISPD-Copy Map Point"
+
+Use:
+1. Show the toolbar "GISPD-Copy Map Point"
+2. Add at least one data layer to ArcMap
+3. Open the Python window
+4. Click on the tool "GISPD-Copy Map Point"
+5. Click on the map - the lat/long string will appear in the Python window and be available in the paste buffer
+
+Further resources:
+See 2014 Northwest GIS Conference add-in presentation slides available from gispd.com Resources page for help links
+(NW GIS 2014-Howes & Pardy-Extending ArcGIS for Desktop - Python and .NET Add-Ins in a Nutshell-20141015.pdf)
+
+Esri-Generated README text:
+This is a stub project created by the ArcGIS Desktop Python AddIn Wizard.
 
 
 MANIFEST
@@ -20,7 +47,7 @@ makeaddin.py : Running this script to creates or overwrites a .esriaddin file fr
                Optionally, you can copy this file to a well-known folder and use the AddIn manager in ArcMap to point to this location.
 
 config.xml   : The AddIn configuration file containing the CopyMapPoint project properties and AddIn tool properties.
-               This AddIn is for ArcGIS 10.2. To work in ArcGIS 10.1, change the ArcGIS version number to 10.1 and re-create the .esriaddin file.
+               This AddIn is for ArcGIS 10.2 To work in ArcGIS 10.1, change the ArcGIS version number to 10.1 and re-create the .esriaddin file.
 
 Images/*     : All UI images for the project (icons, images for buttons, 
                etc)
